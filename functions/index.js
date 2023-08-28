f093b1b0c112b614600f325bcbf9be1f5e31d523
Keaton Freed <keatonafreed2@icloud.com>
2023-08-28 09:52:27 -0700

    Finished fully working

--- functions/index.js
+++ functions/index.js
@@ -27,14 +27,41 @@ initializeApp();
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 const db = getFirestore();
 
+const ical2json = require("ical2json");
+
+const fetch = require('node-fetch');
+
 // const dotenv = require('dotenv').config()
 const runAlerts = (req, res) => {
+    const PERSONAL_PHONE = "+19255886990"
+
+    const convert = async (fileLocation) => {
+        const icsRes = await fetch(fileLocation)
+        const icsData = await icsRes.text()
+
+        const data = ical2json.convert(icsData)
+        return data
+    }
+    function parseICalDate(date) {
+        const year = date.substr(0, 4);
+        const month = parseInt(date.substr(4, 2), 10) - 1;
+        const day = date.substr(6, 2);
+        const hour = date.substr(9, 2);
+        const minute = date.substr(11, 2);
+        const second = date.substr(13, 2);
+        return new Date(Date.UTC(year, month, day, hour, minute, second));
+    }
+    function datesAreOnSameDay(first, second) {
+        return (first.getFullYear() === second.getFullYear() &&
+            first.getMonth() === second.getMonth() &&
+            first.getDate() === second.getDate());
+    }
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = require('twilio')(accountSid, authToken);
     function sendTwilioText(number, text) {
         if (!number || !text) return;
-        let response = client.messages
+        return client.messages
             .create({
                 body: text,
                 from: '+18559444529',
@@ -43,21 +70,59 @@ const runAlerts = (req, res) => {
     }
 
     function alertUser(uid, userData) {
-        if (!uid || !userData) return;
+        if (!uid || !userData)
+            return;
+        if (!userData.phoneNumber || !userData.phoneNumber?.length === 11)
+            return;
+        if (!userData.calenderLink || !userData.calenderLink?.match(/^webcal:\/\/.+\.pike13.com\/[^.]+\.ics/))
+            return;
 
-        if (!userData.phoneNumber || !userData.phoneNumber?.length === 11) return;
+        let icsLink = userData.calenderLink.replace("webcal://", "https://")
+        convert(icsLink).then((icsData) => {
+            if (!icsData || !icsData["VCALENDAR"]?.length || !icsData["VCALENDAR"][0]["VEVENT"].length) {
+                logger.log("err while getting ics data, returned:", icsData)
+                return
+            }
+            const currentDate = new Date()
+            cleanedData = icsData["VCALENDAR"][0]["VEVENT"].map(event => {
+                let eventDate = parseICalDate(event['DTSTART;TZID=America/Los_Angeles'])
+                if (!datesAreOnSameDay(currentDate, eventDate))
+                    return;
+                eventTime = eventDate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }).replace(" ", "").toLowerCase();
+                return { ...event, START_TIME: eventTime }
+            }).filter(e => e != null)
+            const eventHelpIndex = 0
+            function getSessionType(summary) {
+                if (summary.includes("In Person")) return "in person";
+                if (summary.includes("Remote")) return "remote";
+                return undefined
+            }
+            logger.log("Sending alert to:", userData.displayName, userData.phoneNumber, "Data:", cleanedData[eventHelpIndex]?.["SUMMARY"])
+            sendTwilioText(userData.phoneNumber, `Hello${userData.displayName ? " " + userData.displayName.substring(0, 20) : ""}, ${cleanedData.length ? `This is a reminder that you have ${cleanedData.length} session${cleanedData.length > 1 ? "s" : ""} at the Coder School today. Your start time is ${cleanedData[eventHelpIndex]["START_TIME"]}${getSessionType(cleanedData[eventHelpIndex]["SUMMARY"]) ? ", " + getSessionType(cleanedData[eventHelpIndex]["SUMMARY"]) : ""}.` : "This a reminder that you have no sessions at The Coder School today. Relax!"}`);
+        }).catch((error) => {
+            logger.log("ERROR! while getting ics data for:", userData.displayName, "E:", error)
+        })
 
-        logger.log("Sending alert to:", userData.displayName, userData.phoneNumber)
-        sendTwilioText(userData.phoneNumber, `Hello${userData.displayName ? " " + userData.displayName.substring(0, 20) : ""}, this is a reminder that you have a 3:30pm In Person session to start today.`);
     }
 
 
     const usersCollectionRef = db.collection("users");
     usersCollectionRef.get()
         .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
+            let sentMaxAlert = false
+            let usersCount = 0;
+            querySnapshot.forEach((doc, ind) => {
+                usersCount++;
+                if (ind >= 50) {
+                    if (!sentMaxAlert) {
+                        sendTwilioText(PERSONAL_PHONE, `BAD Admin Alert! 50 or more users detected, this is probably bad.`);
+                    }
+                    sentMaxAlert = true
+                    return
+                }
                 alertUser(doc.id, doc.data());
             });
+            sendTwilioText(PERSONAL_PHONE, `Admin Notification! Sending alerts(if valid) to ${usersCount} users.`);
             logger.log("Ended 1")
         })
         .catch((error) => {
@@ -67,9 +132,19 @@ const runAlerts = (req, res) => {
     if (res && res.send) {
         res.send("Running")
     }
+
 }
 
 // every day 00:00
-// exports.scheduled = onSchedule("every day 20:20", runAlerts)
+exports.scheduled = onSchedule("every day 14:30", runAlerts)
+// exports.scheduled = onSchedule("every day 20:32", runAlerts)
 
-exports.testing = onRequest(runAlerts)
+exports.smsReply = onRequest((req, res) => {
+    const { MessagingResponse } = require('twilio').twiml;
+    const twiml = new MessagingResponse();
+    twiml.message('Thanks for the message, but this is a noreply number. Please type STOP to stop these messages.');
+
+    logger.log("got message", req.body)
+    res.type('text/xml').send(twiml.toString());
+
+})
